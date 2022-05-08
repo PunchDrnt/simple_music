a696d80683b6505b99923948270ace5fd8bb298a
PunchDrnt <punch.drnt@gmail.com>
2022-05-08 15:48:59 +0700

    main

---
+++ pages/track/[id].js
@@ -0,0 +1,6 @@
+// import { useRouter } from 'next/router'
+// export default function TrackPage() {
+//   const router = useRouter()
+//   console.log(router.query.id)
+//   const apiUrl = `https://api.spotify.com/v1/tracks/${router.query.id}`
+// }
