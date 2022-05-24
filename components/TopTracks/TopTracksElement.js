621405af2d48dce74905757a6e3888887fd468db
PunchDrnt <punch.drnt@gmail.com>
2022-05-24 10:48:03 +0700

    NewTrackStyled

--- components/TopTracks/TopTracksElement.js
+++ components/TopTracks/TopTracksElement.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+// index.js
 export const TrackContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -9,22 +10,19 @@ export const TrackContainer = styled.div`
 `
 
 export const TrackWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 4rem;
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `
 
+// Track.js
 export const TracksCard = styled.div`
   display: flex;
-  border-radius: 10px;
-  justify-content: flex-start;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
-  transition: all 0.2 ease-in-out;
   align-items: center;
-  width: 400px;
-  height: 200px;
-  padding: 30px;
+  border-bottom: solid 1px rgba(9, 9, 9, 0.6);
+  width: 700px;
+  margin-top: 10px;
+  padding-bottom: 10px;
 
   &:hover {
     transform: scale(1.02);
@@ -42,18 +40,12 @@ export const TrackRank = styled.h2`
 export const TrackDetail = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `
 
 export const TrackTitle = styled.p`
   font-size: 18px;
-  margin: 12px;
-  text-align: center;
 `
 
 export const TrackArtist = styled.p`
-  font-size: 14px;
-  margin: 12px;
-  text-align: center;
+  font-size: 12px;
 `
