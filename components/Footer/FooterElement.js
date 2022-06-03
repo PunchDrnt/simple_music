968eb874e3e302ba6d3f903d914de7ea36eb4699
PunchDrnt <punch.drnt@gmail.com>
2022-06-03 16:44:15 +0700

    new footer

--- components/Footer/FooterElement.js
+++ components/Footer/FooterElement.js
@@ -19,19 +19,7 @@ export const FooterWrapper = styled.div`
 `
 
 export const FooterWrap = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 10rem;
   margin: 0 auto;
   padding: 0 50px;
   max-width: 1000px;
-`
-export const FooterMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  max-height: 340;
-  padding: 30px;
-  transition: all 0.2 ease-in-out;
 `
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,32 +1,11 @@
 import { NowPlaying } from '@components/NowPlaying'
-import {
-  FooterCotainer,
-  FooterMenu,
-  FooterWrapper,
-  FooterWrap,
-} from './FooterElement'
+import { FooterCotainer, FooterWrapper, FooterWrap } from './FooterElement'
 const Footer = () => {
   return (
     <FooterCotainer>
       <FooterWrapper>
         <NowPlaying />
-        <FooterWrap>
-          <FooterMenu>
-            <p>text</p>
-            <p>text</p>
-            <p>text</p>
-          </FooterMenu>
-          <FooterMenu>
-            <p>text2</p>
-            <p>text2</p>
-            <p>text2</p>
-          </FooterMenu>
-          <FooterMenu>
-            <p>text3</p>
-            <p>text3</p>
-            <p>text3</p>
-          </FooterMenu>
-        </FooterWrap>
+        <FooterWrap></FooterWrap>
       </FooterWrapper>
     </FooterCotainer>
   )
